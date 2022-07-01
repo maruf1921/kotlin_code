3b84802ebc1f9f82675adae60023536afeb59e80
maruf <maruf1921@gmail.com>
2022-07-01 19:23:40 +0600

    cal1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+
+    print("enter the first number:")
+    val firstNo : String = readln()!!
+
+    println("pleas enter your operator:")
+    val operator : String = readLine()!!
+
+
+    println("enter the second number:")
+    val secondNo : String = readln()!!
+
+    var res : Int = 0
+    if (operator ==  "+"){
+        res = firstNo.toInt() + secondNo.toInt()
+    }
+    else if (operator == "-" ){
+        res = firstNo.toInt() - secondNo.toInt()
+    }
+    else if (operator == "*" ){
+        res = firstNo.toInt() * secondNo.toInt()
+    }
+    else if (operator == "/" ){
+        res = firstNo.toInt() / secondNo.toInt()
+    }else {
+        println("operator error !")
+    }
+
+  //  val res : Int = firstNo.toInt() - secondNo.toInt()
+
+    println("Result is: $res")
+}
